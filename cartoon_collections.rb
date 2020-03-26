5c421e285b97fb73ab0b72964640071f1bb66e84
Ryan Parrish <rino3717@gmail.com>
2020-03-26 17:12:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def long_planeteer_calls(calls) # code an argument here
   calls.any? { |word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(strings)# code an argument here
+ 
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
