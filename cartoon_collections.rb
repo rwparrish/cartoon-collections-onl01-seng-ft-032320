a3f42d4744e17afb9da7c5c3358a8fe60a8334d5
Ryan Parrish <rino3717@gmail.com>
2020-03-26 17:02:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,6 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)# code an argument here
+  names.each_with_index(1)
+  
 end
 
 def summon_captain_planet# code an argument here
