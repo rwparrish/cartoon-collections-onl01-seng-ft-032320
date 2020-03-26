9f28199cb5f2bf41b4656382bf92e03d20ab2a2f
Ryan Parrish <rino3717@gmail.com>
2020-03-26 17:36:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,7 @@ end
 
 def find_the_cheese(strings)# code an argument here
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if strings.find {|cheese| cheese = cheddar}
+  if strings.find {|cheese| cheese = cheese_types}
   else
   end
 end
